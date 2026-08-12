a4483f760cda3a3a0d063578926fa5169ae9cf40
jxzhang <zjx119009@163.com>
2026-08-12 21:43:37 +0800

    [feat] prepare to upgrade least square

--- control_lib/include/parameter_estimator.hpp
+++ control_lib/include/parameter_estimator.hpp
@@ -7,17 +7,17 @@
 #include "typing.hpp"
 
 
-template<typename DataType, int N_theta, int N_step>
+template<typename DataType, int N_theta, int N_measurement>
 class LeastSquareEstimator_WeightBatch
 {
 
 public:
 
-    using RegressorMatrix = Matrix<DataType, N_step, N_theta>;
+    using RegressorMatrix = Matrix<DataType, N_measurement, N_theta>;
     using ParameterVector = Vector<DataType, N_theta>;
-    using MeasurementVector = Vector<DataType, N_step>;
-    using WeightVector = Vector<DataType, N_step>;
-    using WeightDiagonalMatrix = Eigen::DiagonalMatrix<DataType, N_step>;
+    using MeasurementVector = Vector<DataType, N_measurement>;
+    using WeightVector = Vector<DataType, N_measurement>;
+    using WeightDiagonalMatrix = Eigen::DiagonalMatrix<DataType, N_measurement>;
 
 
     const RegressorMatrix& GetRegressor() const{
@@ -62,6 +62,8 @@ public:
 
     void Compute() {
         const WeightDiagonalMatrix weight_matrix(this->weight_vector_);
+
+        // Ax = b <=> (A).ldlt().solve(b)
         this->parameter_ =
             (this->regressor_.transpose() * weight_matrix * this->regressor_).ldlt().solve(
                 this->regressor_.transpose() * weight_matrix * this->measurement_);
@@ -78,135 +80,64 @@ private:
 };
 
 
-
 template<typename DataType, int N_theta>
-class LeastSquareEstimator_Recursive
-{
-
+class GrandientEstimator_Standard{
 public:
+
 
     using RegressorVector = Vector<DataType, N_theta>;
     using ParameterVector = Vector<DataType, N_theta>;
-    using GainVector = Vector<DataType, N_theta>;
-    using CovarianceMatrix = Matrix<DataType, N_theta, N_theta>;
 
 
-    const RegressorVector& GetRegressor() const {
+    const RegressorVector& GetRegressor() const{
         return this->regressor_;
+
     }
 
-    const ParameterVector& GetParameter() const {
+    const ParameterVector& GetParameter() const{
         return this->parameter_;
     }
 
-    const GainVector& GetGain() const {
-        return this->K_;
+    const float GetTs() const{
+        return this->Ts_;
     }
 
-    const CovarianceMatrix& GetCovariance() const {
-        return this->covariance_;
-    }
-
-    DataType GetMeasurement() const {
-        return this->measurement_;
-    }
-
-    DataType GetInnovation() const {
-        return this->innovation_;
-    }
-
-    DataType GetForgettingFactor() const {
-        return this->forgetting_factor_;
-    }
-
-    void SetRegressor(const RegressorVector& regressor) {
+    void SetRegressor(const RegressorVector& regressor){
         this->regressor_ = regressor;
     }
 
-    void SetMeasurement(DataType measurement) {
+    void SetMeasurement(const RegressorVector& measurement){
         this->measurement_ = measurement;
     }
 
-    void SetParameter(const ParameterVector& parameter) {
-        this->parameter_ = parameter;
+    void SetTs(const float Ts){
+        this->Ts_ = Ts;
     }
 
-    void SetCovariance(const CovarianceMatrix& covariance) {
-        this->covariance_ = covariance;
-    }
-
-    bool SetForgettingFactor(DataType forgetting_factor) {
-        if ((forgetting_factor <= static_cast<DataType>(0)) ||
-            (forgetting_factor > static_cast<DataType>(1)))
-        {
-            return false;
-        }
-
-        this->forgetting_factor_ = forgetting_factor;
-        return true;
-    }
-
-    void Reset() {
+    void Reset(){
         this->regressor_.setZero();
         this->parameter_.setZero();
-        this->K_.setZero();
-        this->covariance_.setIdentity();
-        this->measurement_ = static_cast<DataType>(0);
-        this->innovation_ = static_cast<DataType>(0);
-        this->forgetting_factor_ = static_cast<DataType>(1);
+        this->measurement_.setZero();
     }
 
-    void Compute() {
-        const DataType denominator =
-            this->forgetting_factor_ +
-            (this->regressor_.transpose() * this->covariance_ * this->regressor_)(0, 0);
+    void Compute(){
 
-        this->K_ = this->covariance_ * this->regressor_ / denominator;
-        this->innovation_ =
-            this->measurement_ - (this->regressor_.transpose() * this->parameter_)(0, 0);
-        this->parameter_ += this->K_ * this->innovation_;
-        this->covariance_ =
-            (CovarianceMatrix::Identity() - this->K_ * this->regressor_.transpose()) *
-            this->covariance_ / this->forgetting_factor_;
+        this->
+
     }
+
+
+
 
 
 private:
 
+
     RegressorVector regressor_{RegressorVector::Zero()};
     ParameterVector parameter_{ParameterVector::Zero()};
-    GainVector K_{GainVector::Zero()};
-    CovarianceMatrix covariance_{CovarianceMatrix::Identity()};
+    ParameterVector dot_parameter_{ParameterVector::Zero()};
+    float Ts_{0};
 
-    DataType measurement_{static_cast<DataType>(0)};
-    DataType innovation_{static_cast<DataType>(0)};
-    DataType forgetting_factor_{static_cast<DataType>(1)};
-
-};
-
-
-template<typename DataType, int N_theta, int N_step>
-class RegressorBuilder_FIRBatch{
-
-};
-
-
-template<typename DataType, int N_theta, int N_step>
-class RegressorBuilder_FIRRecursive{
-
-};
-
-
-template<typename DataType, int N_theta, int N_step>
-class RegressorBuilder_OutputErrorRecursive{
-
-};
-
-
-
-template<typename DataType, int N_theta, int N_step>
-class RegressorBuilder_EquationErrorRecursive{
-
-};
+}
 
 
